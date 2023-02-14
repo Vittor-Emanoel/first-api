32571cd428b471ec335df1d7c0b6b79b143c9144
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-14 15:17:51 -0300

    query params

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 const http = require('http')
 
+const { URL } = require('url')
 const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+  const parsedUrl = new URL(`http://localhost:3333${request.url}`)
+
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+  )
 
   const route = routes.find((routeObj) => {
     return (
-      routeObj.endpoint === request.url && routeObj.method === request.method
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method
     )
   })
 
   if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams)
     route.handler(request, response)
   } else {
     response.writeHead(404, { 'Content-Type': 'text/html' })
-    response.end(`Cannot ${request.method} ${request.url}`)
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
   }
 })
 
