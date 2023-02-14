ca7d582fc5ac67c83c0f698aa2a0b9ffa6228b04
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-02-13 22:19:40 -0300

    criando primeira rota da API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const http = require('http')
-const { text } = require('stream/consumers')
+
+const users = require('./mocks/users')
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' })
-  response.end('<h1>Hello World</h1>')
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+
+  if (request.url === '/users' && request.method === 'GET') {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(users))
+  }
+  //   response.writeHead(200, { 'Content-Type': 'text/html' })
+  // response.end('<h1>Hello World</h1>')
 })
 
 server.listen(3333, () =>
---
+++ src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Vittor',
+  },
+  {
+    id: 2,
+    name: 'Emanoel',
+  },
+  {
+    id: 3,
+    name: 'Ramos',
+  },
+  {
+    id: 4,
+    name: 'Silva',
+  },
+  {
+    id: 5,
+    name: 'Noah',
+  },
+]
